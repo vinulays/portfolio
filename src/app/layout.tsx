1da5aefbc7e51aa529180ae1ddbe96681baf5e24
Vinula Senarathne <vinulapc@gmail.com>
2024-09-16 09:30:37 +0530

    project init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,15 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Inter, Calistoga } from "next/font/google";
 import "./globals.css";
+import { twMerge } from "tailwind-merge";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+
+const calistoga = Calistoga({
+  subsets: ["latin"],
+  variable: "--font-serif",
+  weight: ["400"],
 });
 
 export const metadata: Metadata = {
@@ -26,7 +25,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={twMerge(
+          inter.variable,
+          calistoga.variable,
+          "bg-gray-900 text-white antialiased font-sans"
+        )}
       >
         {children}
       </body>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,22 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1200px",
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        md: "2rem",
+      },
+    },
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        sans: "var(--font-sans)",
+        serif: "var(--font-serif)",
       },
     },
   },
