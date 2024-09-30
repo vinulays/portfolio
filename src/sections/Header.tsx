df20ae1e7f20c289b270ec89c558996902c97a04
Vinula Senarathne <vinulapc@gmail.com>
2024-09-30 16:43:48 +0530

    feat: added schema

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Head from "next/head";
 import { useEffect, useState } from "react";
 
 const Header = () => {
@@ -30,50 +31,72 @@ const Header = () => {
   }, []);
 
   return (
-    <div className="flex justify-center items-center fixed top-3 w-full z-10">
-      <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
-        <a
-          href="#"
-          className={`nav-item ${
-            activeSection === "home"
-              ? "bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
-              : ""
-          } `}
-        >
-          Home
-        </a>
-        <a
-          href="#projects"
-          className={`nav-item ${
-            activeSection === "projects"
-              ? "bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
-              : ""
-          } `}
-        >
-          Projects
-        </a>
-        <a
-          href="#about"
-          className={`nav-item ${
-            activeSection === "about"
-              ? "bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
-              : ""
-          } `}
-        >
-          About
-        </a>
-        <a
-          href="#contact"
-          className={`nav-item ${
-            activeSection === "contact"
-              ? "bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
-              : ""
-          } `}
-        >
-          Contact
-        </a>
-      </nav>
-    </div>
+    <>
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org/",
+              "@type": "Person",
+              name: "Vinula Senarathne",
+              url: "https://vinulasenarathne.vercel.app/",
+              image: "",
+              sameAs: [
+                "https://www.linkedin.com/in/vinula-senarathne",
+                "https://github.com/vinulays",
+                "https://www.facebook.com/vinulasen",
+              ],
+              jobTitle: "Full Stack Developer",
+            }),
+          }}
+        />
+      </Head>
+      <div className="flex justify-center items-center fixed top-3 w-full z-10">
+        <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+          <a
+            href="#"
+            className={`nav-item ${
+              activeSection === "home"
+                ? "bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
+                : ""
+            } `}
+          >
+            Home
+          </a>
+          <a
+            href="#projects"
+            className={`nav-item ${
+              activeSection === "projects"
+                ? "bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
+                : ""
+            } `}
+          >
+            Projects
+          </a>
+          <a
+            href="#about"
+            className={`nav-item ${
+              activeSection === "about"
+                ? "bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
+                : ""
+            } `}
+          >
+            About
+          </a>
+          <a
+            href="#contact"
+            className={`nav-item ${
+              activeSection === "contact"
+                ? "bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
+                : ""
+            } `}
+          >
+            Contact
+          </a>
+        </nav>
+      </div>
+    </>
   );
 };
 
